b2301bb55bead5ee3ca94bdd76831e17b6827de5
Юрий Данильченко <genitra111@gmail.com>
2022-08-17 19:02:54 +1000

    Готовое домашнее задание

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+
+        String demo1 = bins.sum("100", "110");
+        System.out.println(demo1);
+
+        String demo2 = bins.mult("100", "110");
+        System.out.println(demo2);
+
+    }
+}
